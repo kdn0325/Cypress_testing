b90aa30a3fa59750df4ad7d071ebfe69bf145c06
Abel <dn10003@gmail.com>
2023-04-23 00:52:27 +0900

    #2_Cypress@12.6

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    /* *는 지정된 이름과 관계없이 파일을 찾음 */
+    specPattern : "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}"
   },
 });
