c38408e6122e2a9b6c15dac60c4d333a6b460cb7
Abel <dn10003@gmail.com>
2023-04-25 23:54:58 +0900

    #3_contact_test

--- cypress/e2e/tests/contact-us.js
+++ cypress/e2e/tests/contact-us.js
@@ -3,17 +3,28 @@
 
 /* describe의 두번째 인자는 callback  */
 
-describe("Test Contack Us form via WebdriverUni",()=> {
+describe("WebdriverUniversity.com에서 테스트 코드 제출",()=> {
     /* 개별 테스트는 it */
-    it("Should be able to submit a successful submisson via contact us form",()=>{
+    it("연락처를 테스트 코드로 제출",()=>{
         //cypress code
-        cy.visit('http://www.webdriveruniversity.com/ ');
-        cy.get('#contact-us > .thumbnail').click();
+        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+        // cy.get('#contact-us').click({force:true});
+        cy.get('[name="first_name"]').type("Kim");
+        cy.get('[name="last_name"]').type("Abel");
+        cy.get('[name="email"]').type("dn10003@gmail.com");
+        cy.get('textarea.feedback-input').type("안녕하세요 필드 전송 테스트입니다");
+        cy.get('[type="submit"]').click()
 
 
     });
-    it("Should not be able to submit a successful submisson via contact us form as all field required",()=>{
+    it("연락처를 테스트 코드로 비정상적인 제출",()=>{
         //cypress code
+        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+        // cy.get('#contact-us').click({force:true});
+        cy.get('[name="first_name"]').type("Kim");
+        cy.get('[name="last_name"]').type("Abel");
+        cy.get('textarea.feedback-input').type("안녕하세요 필드 전송 테스트입니다");
+        cy.get('[type="submit"]').click()
     });
 
 });
