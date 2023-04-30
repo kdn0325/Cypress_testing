88fbc33f272bb95bdc9d620a87fb92a953e20aa1
Abel <dn10003@gmail.com>
2023-04-30 23:53:28 +0900

    #4_Autumation-test-store Submit

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -13,14 +13,21 @@ describe("Test Store에서 테스팅 자동화",()=> {
         cy.get('#ContactUsFrm_email').type('dn10003@gmail.com');
         cy.get('#ContactUsFrm_enquiry').type('대량 주문 시 추가 할인을 제공합니까?');
         cy.get('.col-md-6 > .btn').click();
-        cy.get('.col-md-6 > .btn').click();
 
         //webdriver -unitest
     });
 });
 
 describe('', () => {
-    it('', () => {
-        
+    it("연락처를 테스트 코드로 제출",()=>{
+        //cypress code
+        cy.visit('https://automationteststore.com/');
+        cy.get('.info_links_footer > :nth-child(5) > a').click();
+        cy.get('#ContactUsFrm_first_name').type('Abel');
+        cy.get('#ContactUsFrm_email').type('dn10003@gmail.com');
+        cy.get('#ContactUsFrm_enquiry').type('대량 주문 시 추가 할인을 제공합니까?');
+        cy.get('.col-md-6 > .btn').click();
+
+        //webdriver -unitest
     });
 })
